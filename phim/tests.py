6e7f4112d8785e4fa85f1833010d6708f028ba07
nguyenducngoc <wangyuhi167@gmail.com>
2023-05-06 07:12:59 +0700

    Test Case

--- phim/tests.py
+++ phim/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.contrib.auth import authenticate, get_user_model
 
-# Create your tests here.
+User = get_user_model()
+
+class LoginTestCase(TestCase):
+    
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='ngocnd',
+            email='123@dmail.com',
+            password='ngocbityto'
+        )
+        
+    def test_login_success(self):
+        user = authenticate(username='testuser', password='testpass')
+        self.assertIsNotNone(user)
+        self.assertEqual(user, self.user)
+        
+    def test_login_failure(self):
+        user = authenticate(username='testuser', password='wrongpass')
+        self.assertIsNone(user)
